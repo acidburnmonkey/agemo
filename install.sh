8d4ebb111211f5f48764c580c84413da943df022
acidburnmonkey <acidburnmonkey@gmail.com>
2024-12-28 02:09:25 -0500

    stop tracking agemo.json

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##
+## https://github.com/acidburnmonkey/agemo
+##
+
+
+
+
+mkdir -p "$HOME/.local/share/agemo"
+cp  -r assets/ agemo.json thumnailer.py HyprParser.py agemo.py  "$HOME/.local/share/agemo"
+
+sed -i "s|{}|${HOME}|g" agemo.desktop
+cp agemo.desktop "$HOME/.local/share/applications/"
+
+chmod +x "$HOME/.local/share/agemo/agemo.py"
+
+pip install -r requirements.txt
+
