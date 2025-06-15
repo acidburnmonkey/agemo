2d6900df7da9824d169c7dd681f93c5a1ccc15b7
acidburnmonkey <acidburnmonkey@gmail.com>
2025-06-15 00:04:38 -0400

    added shared data

--- main.py
+++ main.py
@@ -1,17 +1,87 @@
 import sys
+import os
+import subprocess
+import json
 import PyQt6.QtWidgets as qt
 from PyQt6.QtCore import Qt
 
 
+class SharedData:
+    '''
+    This class load in memory data that the application uses
+    @ self.data => default_settings {dict}
+    @ self.thumbnails_data => json
+
+    '''
+    def __init__(self):
+
+        self.script_path = os.path.join(os.path.dirname(__file__))
+        self.data = self.load_settings()
+        self.check_monitors()
+        self.thumbnails_data = self.load_thubnailer_data()
+
+
+    def load_settings(self):
+        default_settings = {
+            "monitors": [],
+            "splash": False,
+            "ipc": True,
+            "dpi": None,
+            "wallpapers_dir": None
+        }
+
+        try:
+            with open(os.path.join(self.script_path,'agemo.json'), 'r') as f:
+                file_data = json.load(f)
+                return {**default_settings, **file_data}
+
+        except FileNotFoundError:
+            print("Error: Configuration file not found.")
+            return {}
+        except json.JSONDecodeError:
+            print("Error: Malformed JSON in.")
+            return {}
+
+    def check_monitors(self):
+        try:
+            hypr_ctl = subprocess.run(['hyprctl','monitors','-j'], stdout=subprocess.PIPE, text=True)
+            hold = json.loads(hypr_ctl.stdout)
+
+            # returns list of monitor names
+            monitors = [m.get('name') for m in hold]
+            self.data['monitors'] = monitors
+
+            # Auto populate Monitors
+            with open(os.path.join(self.script_path,'agemo.json'),'w') as f:
+                json.dump(self.data,f, indent=4)
+
+        except Exception as e:
+            print(e)
+
+    def load_thubnailer_data(self):
+        try:
+            with open(os.path.join(self.script_path,'thumbnail_cache.json'), 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print("Error: Configuration file not found.")
+            return {}
+        except json.JSONDecodeError:
+            print("Error: Malformed JSON in.")
+            return {}
+
+
+
 # bottom bar
 class BottomBar(qt.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self,shared_data=None,parent=None):
         super().__init__(parent)
 
 
         # with open('BottomBar.qss','r') as f:
         #     self.css = f.read()
 
+        self.monitors = shared_data.data['monitors']
+        print(self.monitors)
 
         # buttons
         self.apply = qt.QPushButton("Apply")
@@ -52,7 +122,11 @@ class MainWindow(qt.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.bottom_bar = BottomBar(self)
+        #shared data
+        self.shared_data = SharedData()
+        print(str(self.shared_data.data))
+
+        self.bottom_bar = BottomBar(self.shared_data,self)
         self.testLabel = qt.QLabel('TEST')
 
         self.initUi()
