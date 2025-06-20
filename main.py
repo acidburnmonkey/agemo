853395910cd8a706610cae66a2eeabb8890f6c57
acidburnmonkey <acidburnmonkey@gmail.com>
2025-06-20 16:56:22 -0400

    moved SharedData class , setting menu done

--- main.py
+++ main.py
@@ -8,77 +8,13 @@
 
 import sys
 import os
-import subprocess
 import json
 import PyQt6.QtWidgets as qt
 from PyQt6.QtCore import Qt, QSize, QProcess, QProcessEnvironment
 from PyQt6.QtGui import QIcon, QPixmap
 import xdgthumbails
+from SharedData import SharedData
 
-
-class SharedData:
-    """
-    This class load in memory data that the application uses
-    @ self.data => default_settings {dict}
-    @ self.thumbnails_data => json
-    @ self.script_path => gets the directory of this program
-    """
-
-    def __init__(self):
-        self.script_path = os.path.join(os.path.dirname(__file__))
-        self.data = self.load_settings()
-        self.check_monitors()
-        self.thumbnails_data = self.load_thubnailer_data()
-
-    def load_settings(self):
-        default_settings = {
-            "monitors": [],
-            "splash": False,
-            "ipc": True,
-            "dpi": None,
-            "wallpapers_dir": None,
-        }
-
-        try:
-            with open(os.path.join(self.script_path, "agemo.json"), "r") as f:
-                file_data = json.load(f)
-                return {**default_settings, **file_data}
-
-        except FileNotFoundError:
-            print("Error: Configuration file not found.")
-            return {}
-        except json.JSONDecodeError:
-            print("Error: Malformed JSON in.")
-            return {}
-
-    def check_monitors(self):
-        try:
-            hypr_ctl = subprocess.run(
-                ["hyprctl", "monitors", "-j"], stdout=subprocess.PIPE, text=True
-            )
-            hold = json.loads(hypr_ctl.stdout)
-
-            # returns list of monitor names
-            monitors = [m.get("name") for m in hold]
-            self.data["monitors"] = monitors
-
-            # Auto populate Monitors
-            with open(os.path.join(self.script_path, "agemo.json"), "w") as f:
-                json.dump(self.data, f, indent=4)
-
-        except Exception as e:
-            print(e)
-
-    def load_thubnailer_data(self):
-        try:
-            with open(os.path.join(self.script_path, "thumbnail_cache.json"), "r") as f:
-                return json.load(f)
-        except FileNotFoundError:
-            print("Error: Configuration file not found.")
-            return {}
-        except json.JSONDecodeError:
-            print("Error: Malformed JSON in.")
-            return {}
 
 
 # bottom bar
@@ -340,16 +276,25 @@ class SettingsWindow(qt.QWidget):
 
         # checkbox
         if self.shared_data.data["dpi"]:
+            self.checkBox.setChecked(True)
             self.dpiLabel.setText("Disable Dpi Scaling")
         elif not self.shared_data.data["dpi"]:
             self.dpiLabel.setText("Set DPI")
+            self.checkBox.setChecked(False)
+
+        if self.checkBox.isChecked():
+            self.slider.setEnabled(True)
+        else:
+            self.slider.setEnabled(False)
+
 
         ##END UI
         self.setLayout(self.settingsLayout)
 
     # Apply Settings
     def scaleNow(self):
-        if self.shared_data.data["dpi"]:
+
+        if self.shared_data.data["dpi"] and self.checkBox.isChecked():
             # write to config file
             with open(
                 os.path.join(self.shared_data.script_path, "agemo.json"), "w"
@@ -375,6 +320,14 @@ class SettingsWindow(qt.QWidget):
 
             # kill current UI
             qt.QApplication.quit()
+
+        elif not self.checkBox.isChecked():
+            self.shared_data.data['dpi'] = None
+            with open(
+                os.path.join(self.shared_data.script_path, "agemo.json"), "w"
+            ) as f:
+                json.dump(self.shared_data.data, f, indent=4)
+
 
     def slide(self, i):
         val = 1.0 + i * 0.5
