b55a10bf2508a7fbaf33ed94ec7f1817dd2b8b6b
acidburnmonkey <acidburnmonkey@gmail.com>
2025-06-21 00:00:45 -0400

    changed for uv

--- install.sh
+++ install.sh
@@ -4,17 +4,11 @@
 ## https://github.com/acidburnmonkey/agemo
 ##
 
-
-
-
 mkdir -p "$HOME/.local/share/agemo"
-cp  -r assets/ agemo.json thumnailer.py HyprParser.py agemo.py  "$HOME/.local/share/agemo"
+cp  -r assets/ agemo.json *.py style.qss pyproject.toml uv.lock "$HOME/.local/share/agemo"
 
 sed -i "s|{}|${HOME}|g" agemo.desktop
 cp agemo.desktop "$HOME/.local/share/applications/"
 
-chmod +x "$HOME/.local/share/agemo/agemo.py"
-
-pip install -r requirements.txt
-
+chmod +x "$HOME/.local/share/agemo/main.py"
 echo 'Done ,agemo Installed on ~/.local/share/'
