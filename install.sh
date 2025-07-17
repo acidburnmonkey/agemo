b158b6fc47db10c3eeb3c041a0794fa0e570eb43
acidburnmonkey <acidburnmonkey@gmail.com>
2025-07-17 12:00:28 -0400

    added -p .local/share/applications

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 mkdir -p "$HOME/.local/share/agemo"
 cp  -r assets/ agemo.json *.py style.qss pyproject.toml uv.lock "$HOME/.local/share/agemo"
 
+mkdir -p "$HOME/.local/share/applications/"
 sed -i "s|{}|${HOME}|g" agemo.desktop
 cp agemo.desktop "$HOME/.local/share/applications/"
 
